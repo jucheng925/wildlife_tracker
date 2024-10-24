a08e6d1022bb7edc2912ee4be6d3d7df46c7e483
Julie Cheng <jucheng925@gmail.com>
2024-10-24 04:44:17 -0400

    10 most rare hard to see animals data added

---
+++ src/data/animal.jsx
@@ -0,0 +1,73 @@
+const animals = [
+  {
+    name: "Amur Leopard",
+    species: "Panthera Pardus Orientalis",
+    conservation_status: "Critically Endangered",
+    region: [ "Far East Russia, Northeastern China"],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDeYCdLs-4lSrAzy1KZ4oJ_7NiAZy4huwVLIR4PBqVNC--TF0N"
+  },
+  {
+    name: "Vaquita",
+    species: "Phocoena sinus",
+    conservation_status: "Critically Endangered",
+    region: ["Northern Gulf of California", "Mexico"],
+    img: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTw9XZyJ7SW5UmNropmaslWHyDNSglwIrSAWG7RaJPNjBKE2ccx"
+  },
+  {
+    name: "Javan Rhino",
+    species: "Rhinoceros sondaicus",
+    conservation_status: "Critically Endangered",
+    region: ["Ujung Kulon National Park", "Indonesia"],
+    img: "https://rhinos.org/wp-content/uploads/2020/09/Hero-BlackRhino.jpg"
+  },
+  {
+    name: "Saola",
+    species: "Pseudoryx nghetinhensis",
+    conservation_status: "Critically Endangered",
+    region: ["Annamite Mountains", "Laos", "Vietnam"],
+    img: "https://www.synchronicityearth.org/wp-content/uploads/2023/10/By-Toon-Fey39-saola.jpg"
+  },
+  {
+    name: "Cross River Gorilla",
+    species: "Gorilla gorilla diehli",
+    conservation_status: "Critically Endangered",
+    region: ["Nigeria", "Cameroon"],
+    img: "https://gorillas.org/wp-content/uploads/gorillaorg-crossriver-gorilla-main.jpg"
+  },
+  {
+    name: "Snow Leopard",
+    species: "Panthera uncia",
+    conservation_status: "Vulnerable",
+    region: ["Central Asia (Himalays, Altai Mountains"],
+    img: "https://animalfactguide.com/wp-content/uploads/2024/01/snow-leopard2.jpg"
+  },
+  {
+    name: "Iberian Lynx",
+    species: "Lynx pardinus",
+    conservation_status: "Endangered",
+    region: ["Spain", "Portugal"],
+    img: "https://assets.unenvironment.org/decadeonrestoration/s3fs-public/2024-06/Iberian%20lynx%2C%20Lynx%20pardinus%E2%81%A0%20by%20StockPhotoAstur%20from%20Getty%20Images.png"
+  },
+  {
+    name: "Pangolin",
+    species: "All species in the genus Manis",
+    conservation_status: "Critically Endangered",
+    region: ["Africa", "Asia"],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCdDGOB2yZRq0jOtK5CZfPSqr309Tt0ta_s9xHNQc8rgL5QiBx"
+  },
+  {
+    name: "Aye-Aye",
+    species: "Daubentonia madagascariensis",
+    conservation_status: "Endangered",
+    region: ["Madagascar"],
+    img: "https://i.ytimg.com/vi/cQO3bU797lY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB6SFkxpRlnagZsCr7HmkqhIw5u3Q"
+  },
+  {
+    name: "Yangtze Giant Softshell Turtle",
+    species: "Rafetus swinhoei",
+    conservation_status: "Critically Endangered",
+    region: ["China", "Vietnam"],
+    img: "https://static01.nyt.com/images/2015/05/26/science/26TURTLES/26TURTLES-articleLarge.jpg?quality=75&auto=webp&disable=upscale"
+  }
+
+]
